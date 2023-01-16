d53ff59291471fc335e49a9dd1f4f014e4664c5b
plaiaundi <plaiaundi@HZ309807.H012108.NET>
2023-01-16 13:35:24 +0100

    esqueleto menu

--- src/principal/GestorAlmacen.java
+++ src/principal/GestorAlmacen.java
@@ -1,5 +1,7 @@
 package principal;
 
+import java.util.Scanner;
+
 /**
  * 
  * @author ${Arnold Bermell}
@@ -10,6 +12,38 @@ public class GestorAlmacen {
 
 	public void run() {
 		
+		final int venta = 1;
+		final int compra = 2;
+		final int articulos_saludables = 3;
+		final int articulo_mas_caro = 4;
+		final int articulo_menor_stock = 5;
+		final int salir = 0;
+		Scanner sc = new Scanner(System.in);
+		int opcionmenu;
+		
+		do {
+			System.out.println("Eligue una opcion");
+			opcionmenu = sc.nextInt();
+
+			switch(opcionmenu) {
+			case venta:
+				break;
+			case compra:
+				break;
+				
+			case articulos_saludables:
+				break;
+				
+			case articulo_mas_caro:
+				break;
+			
+			case articulo_menor_stock:
+				break;
+				
+			}
+			
+
+		} while (opcionmenu != salir);
 		
 	}
 }
